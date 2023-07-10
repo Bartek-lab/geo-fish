2fd2231af58745cf788d4af76483a0fa79a9cbd5
Bartek-lab <bartosz.benedyczak@gmail.com>
2023-07-10 20:44:12 +0200

    working on tabs and fish lists

--- lib/Widgets/fish_list.dart
+++ lib/Widgets/fish_list.dart
@@ -1,9 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:geo_fish/Widgets/fish_item.dart';
 
-class FishList extends StatelessWidget {
-  FishList({required this.isGlobal, super.key});
+class FishList extends StatefulWidget {
+  const FishList({required isGlobal, super.key});
+
+  @override
+  State<FishList> createState() => _FishListState();
+}
+
+class _FishListState extends State<FishList> {
+  String currentUserId = "";
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      currentUserId = FirebaseAuth.instance.currentUser!.uid;
+    });
+  }
 
   bool isGlobal = true;
   @override
@@ -11,10 +27,12 @@ class FishList extends StatelessWidget {
     final CollectionReference collectionRef =
         FirebaseFirestore.instance.collection('fishList');
 
-    final data =
-        collectionRef.where("isSharedOnMainList", isEqualTo: isGlobal).get();
     return StreamBuilder(
-      stream: collectionRef.snapshots(),
+      //TODO distingush list  -> author
+      stream: collectionRef
+          .where("author", isEqualTo: currentUserId)
+          .where("isSharedOnMainList", isEqualTo: !isGlobal)
+          .snapshots(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           return ListView.builder(
